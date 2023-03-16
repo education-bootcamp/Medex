bcc4d9f14985108504cc376d0670ac634b738b91
ozen-hub <info.ozenhub@gmail.com>
2023-03-16 21:05:31 +0530

    2

--- src/com/developersstack/medex/controller/LoginFormController.java
+++ src/com/developersstack/medex/controller/LoginFormController.java
@@ -3,6 +3,7 @@ package com.developersstack.medex.controller;
 import com.developersstack.medex.db.Database;
 import com.developersstack.medex.dto.UserDto;
 import com.developersstack.medex.enums.AccountType;
+import com.developersstack.medex.util.Cookie;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXRadioButton;
 import com.jfoenix.controls.JFXTextField;
@@ -21,7 +22,7 @@ public class LoginFormController {
     public JFXRadioButton rBtnDoctor;
     public AnchorPane loginContext;
 
-    public void signInOnAction(ActionEvent actionEvent) {
+    public void signInOnAction(ActionEvent actionEvent) throws IOException {
         String email = txtEmail.getText();
         String password = txtPassword.getText();
         AccountType accountType = rBtnDoctor.isSelected() ? AccountType.DOCTOR : AccountType.PATIENT;
@@ -31,7 +32,12 @@ public class LoginFormController {
             if (dto.getEmail().trim().toLowerCase().equals(email)) {
                 if (dto.getPassword().equals(password)) {
                     if (dto.getAccountType().equals(accountType)) {
-                        new Alert(Alert.AlertType.CONFIRMATION, " Success!").show();
+                        new Alert(Alert.AlertType.CONFIRMATION, "Success!").show();
+                        Cookie.selectedUser=dto;
+                        Stage stage =(Stage) loginContext.getScene().getWindow();
+                        stage.setScene(new Scene(FXMLLoader.
+                                load(getClass().getResource("../view/DoctorDashboardForm.fxml"))));
+                        stage.centerOnScreen();
                         return;
                     } else {
                         /*new Alert(Alert.AlertType.WARNING,

7bb6ec529b69159d44b2e566704b3bbde284b0c8
ozen-hub <info.ozenhub@gmail.com>
2023-03-16 21:40:31 +0530

    4

--- src/com/developersstack/medex/controller/DoctorDashboardFormController.java
+++ src/com/developersstack/medex/controller/DoctorDashboardFormController.java
@@ -1,14 +1,20 @@
 package com.developersstack.medex.controller;
 
 import com.developersstack.medex.util.Cookie;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 public class DoctorDashboardFormController {
@@ -29,11 +35,23 @@ public class DoctorDashboardFormController {
         String today = simpleDateFormat.format(date);
         lblDate.setText(today);*/
         lblDate.setText(new SimpleDateFormat("yyyy-MM-dd").format(new Date()));
+        Timeline timeline = new Timeline(
+                new KeyFrame(Duration.seconds(0),
+                        e -> {
+                            DateTimeFormatter dtf = DateTimeFormatter
+                                    .ofPattern("hh:mm:ss");
+                            lblTime.setText(LocalTime.now().format(dtf));
+                        }
+                ),
+                new KeyFrame(Duration.seconds(1))
+        );
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
     }
 
     public void checkUser() throws IOException {
-        if (null == Cookie.selectedUser){
-            Stage stage =(Stage) doctorDashboardContext.getScene().getWindow();
+        if (null == Cookie.selectedUser) {
+            Stage stage = (Stage) doctorDashboardContext.getScene().getWindow();
             stage.setScene(new Scene(FXMLLoader.
                     load(getClass().getResource("../view/LoginForm.fxml"))));
             stage.centerOnScreen();

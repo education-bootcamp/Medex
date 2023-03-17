6205c71ad5e09fd3cc46b63399d9687717aa0611
ozen-hub <info.ozenhub@gmail.com>
2023-03-17 22:07:57 +0530

    10

--- src/com/developersstack/medex/db/Database.java
+++ src/com/developersstack/medex/db/Database.java
@@ -1,17 +1,21 @@
 package com.developersstack.medex.db;
 
 import com.developersstack.medex.dto.DoctorDto;
+import com.developersstack.medex.dto.PatientDto;
 import com.developersstack.medex.dto.UserDto;
 import com.developersstack.medex.enums.AccountType;
 import com.developersstack.medex.enums.GenderType;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Database {
     //------------
     public static ArrayList<UserDto> userTable =
             new ArrayList();
     public static ArrayList<DoctorDto> doctorTable =
+            new ArrayList<>();
+    public static ArrayList<PatientDto> patientTable =
             new ArrayList<>();
     //------------
 
@@ -35,6 +39,21 @@ public class Database {
                         GenderType.MALE
                 )
         );*/
+        patientTable.add(
+                new PatientDto("95","Hasika","Sandaruwan",
+                        new Date(),GenderType.MALE,"Galle","hasika@gmail.com"));
+        patientTable.add(
+                new PatientDto("124","Samantha","Bandara",
+                        new Date(),GenderType.MALE,"Aluthgama","samantha@gmail.com"));
+        patientTable.add(
+                new PatientDto("452","Namal","Chandana",
+                        new Date(),GenderType.MALE,"Kalutara","namal@gmail.com"));
+        patientTable.add(
+                new PatientDto("457","Wasantha","nihal",
+                        new Date(),GenderType.MALE,"Wadduwa","wasantha@gmail.com"));
+        patientTable.add(
+                new PatientDto("8745","Banda","samanmal",
+                        new Date(),GenderType.MALE,"Panadura","banda@gmail.com"));
         //----------------
 
     }

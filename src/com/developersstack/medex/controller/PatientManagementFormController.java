a60c829ec767599d613dbf4fe0d0a09e603f475b
ozen-hub <info.ozenhub@gmail.com>
2023-03-17 22:28:53 +0530

    13

--- src/com/developersstack/medex/controller/PatientManagementFormController.java
+++ src/com/developersstack/medex/controller/PatientManagementFormController.java
@@ -1,4 +1,24 @@
 package com.developersstack.medex.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 public class PatientManagementFormController {
+    public TextField txtSearch;
+    public AnchorPane patientContext;
+    public TableView tblPatients;
+    public TableColumn colNic;
+    public TableColumn colFirstName;
+    public TableColumn colLastName;
+    public TableColumn colDob;
+    public TableColumn colGender;
+    public TableColumn colAddress;
+    public TableColumn colAge;
+    public TableColumn colEmail;
+
+    public void backToHomeOnAction(ActionEvent actionEvent) {
+    }
 }

d06dc73a8f4673257dcea5e62a6d0bbc6ad6534d
ozen-hub <info.ozenhub@gmail.com>
2023-03-19 23:17:56 +0530

    26

--- src/com/developersstack/medex/util/CrudUtil.java
+++ src/com/developersstack/medex/util/CrudUtil.java
@@ -7,7 +7,20 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class CrudUtil {
-    private static PreparedStatement execute(String sql,Object...params) throws SQLException, ClassNotFoundException {
+
+    private static <T> T execute(String sql,Object...params) throws SQLException, ClassNotFoundException {
+        PreparedStatement pstm = DBConnection.getInstance().getConnection().prepareStatement(sql);
+        for (int i = 0; i < params.length; i++) {
+            pstm.setObject((i + 1), params[i]);
+        }
+        if (sql.startsWith("SELECT")) {
+            return (T) pstm.executeQuery();
+        }
+        return (T) (Boolean) (pstm.executeUpdate() > 0);
+    }
+
+
+    /*private static PreparedStatement execute(String sql,Object...params) throws SQLException, ClassNotFoundException {
         PreparedStatement pstm = DBConnection.getInstance().getConnection().prepareStatement(sql);
         for (int i = 0; i < params.length; i++) {
             pstm.setObject((i+1),params[i]);
@@ -19,5 +32,5 @@ public class CrudUtil {
     }
     public static ResultSet executeQuery(String sql, Object...param) throws SQLException, ClassNotFoundException {
         return execute(sql, param).executeQuery();
-    }
+    }*/
 }

3cdbf7568346e1046169aa825468294903db5379
ozen-hub <info.ozenhub@gmail.com>
2023-03-19 21:32:11 +0530

    20

--- src/com/developersstack/medex/util/PasswordConfig.java
+++ src/com/developersstack/medex/util/PasswordConfig.java
@@ -1,7 +1,15 @@
 package com.developersstack.medex.util;
 
 
+import org.mindrot.BCrypt;
 
 public class PasswordConfig {
+
+    public String encrypt(String text){
+        return BCrypt.hashpw(text,BCrypt.gensalt(10));
+    }
+    public boolean encrypt(String rowPassword, String encryptedPassword){
+        return BCrypt.checkpw(rowPassword,encryptedPassword);
+    }
 
 }

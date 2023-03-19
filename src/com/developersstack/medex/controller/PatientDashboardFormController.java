8e5ce0822b6defc7e5352aca7509243f93cd35e9
ozen-hub <info.ozenhub@gmail.com>
2023-03-19 19:29:27 +0530

    18

---
+++ src/com/developersstack/medex/controller/PatientDashboardFormController.java
@@ -0,0 +1,14 @@
+package com.developersstack.medex.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
+public class PatientDashboardFormController {
+    public AnchorPane patientDashboardContext;
+    public Label lblDate;
+    public Label lblTime;
+
+    public void navigateToNewAppointmentPage(ActionEvent actionEvent) {
+    }
+}

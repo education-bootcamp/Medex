545a1f6f15e63b2693d1b6db4cf785304c259e63
ozen-hub <info.ozenhub@gmail.com>
2023-03-22 23:13:12 +0530

    32

--- src/com/developersstack/medex/controller/NewAppointmentFormController.java
+++ src/com/developersstack/medex/controller/NewAppointmentFormController.java
@@ -1,6 +1,9 @@
 package com.developersstack.medex.controller;
 
+import com.developersstack.medex.util.CrudUtil;
 import com.jfoenix.controls.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -8,14 +11,34 @@ import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-public class NewAppointmentForm {
+public class NewAppointmentFormController {
     public AnchorPane newAppointmentContext;
-    public JFXComboBox cmbDoctors;
+    public JFXComboBox<String> cmbDoctors;
     public JFXDatePicker txtDate;
     public JFXTimePicker txtTime;
     public JFXTextField txtAmount;
     public JFXTextArea txtMessage;
+
+    public void initialize() {
+        setDoctorIds();
+    }
+
+    private void setDoctorIds() {
+        try {
+            ResultSet set = CrudUtil.execute("SELECT doctor_Id,first_name,last_name FROM doctor");
+            ObservableList<String> obList = FXCollections.observableArrayList();
+            while (set.next()) {
+                obList.add(set.getString(1));
+            }
+            cmbDoctors.setItems(obList);
+        } catch (SQLException | ClassNotFoundException e) {
+
+        }
+
+    }
 
     public void backToHomeOnAction(ActionEvent actionEvent) throws IOException {
         setUi("PatientDashboardForm");
--- src/com/developersstack/medex/controller/PatientDashboardFormController.java
+++ src/com/developersstack/medex/controller/PatientDashboardFormController.java
@@ -1,14 +1,28 @@
 package com.developersstack.medex.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class PatientDashboardFormController {
     public AnchorPane patientDashboardContext;
     public Label lblDate;
     public Label lblTime;
 
-    public void navigateToNewAppointmentPage(ActionEvent actionEvent) {
+    public void navigateToNewAppointmentPage(ActionEvent actionEvent) throws IOException {
+        setUi("NewAppointmentForm");
+    }
+
+    private void setUi(String location) throws IOException {
+        Stage stage = (Stage) patientDashboardContext.getScene().getWindow();
+        System.out.println(stage);
+        stage.setScene(new Scene(FXMLLoader.
+                load(getClass().getResource("../view/" + location + ".fxml"))));
+        stage.centerOnScreen();
     }
 }
